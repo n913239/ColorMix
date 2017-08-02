c6a9709c88039a79afbd2389527d93d99dc85109
n913239 <n913239@gmail.com>
2017-08-03 00:48:03 +0800

    研讀 Intro to App Development with Swift 的 Lesson 17  Actions and Outlets ColorMix

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    // switch狀態被改變
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        updateControls()
+        updateColor()
+    }
+    
+    
+    // slider值被改變
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        updateColor()
+    }
+    
+    // 重置按鈕
+    @IBAction func reset(_ sender: Any) {
+        // 重新設定slider的目前數值
+        redSlider.value = 1
+        blueSlider.value = 1
+        greenSlider.value = 1
+        
+        // 關閉switch的開關
+        redSwitch.isOn = false
+        blueSwitch.isOn = false
+        greenSwitch.isOn = false
+        
+        updateControls()
+        updateColor()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +57,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // 更新slider 元件的狀態
+    func updateControls() {
+        redSlider.isEnabled = redSwitch.isOn
+        blueSlider.isEnabled = blueSwitch.isOn
+        greenSlider.isEnabled = greenSwitch.isOn
+    }
+    
+    // 更新colorView 的背景顏色
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        if redSwitch.isOn {
+            red = CGFloat(redSlider.value)
+        }
+        if greenSwitch.isOn {
+            green = CGFloat(greenSlider.value)
+        }
+        if blueSwitch.isOn {
+            blue = CGFloat(blueSlider.value)
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
 
 }
-
